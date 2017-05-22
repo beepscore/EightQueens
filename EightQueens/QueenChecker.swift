aa21adfded3009caa43a66368390fa27474b57e6
beepscore <support@beepscore.com>
2017-05-21 18:42:28 -0700

    Add comment

--- EightQueens/QueenChecker.swift
+++ EightQueens/QueenChecker.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class QueenChecker: NSObject {
 
+    /*
+     Chess has a standard notation for board squares.
+     Could relate row and column numbers to this via an enum.
+     For ease of programming, just use row and column number 0-7.
+
+ */
+
     class func foo() -> Bool {
         return true
     }
