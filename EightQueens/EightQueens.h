33ddc9340922e0a3996adcde94cd37b0acad070e
beepscore <support@beepscore.com>
2017-05-21 17:32:39 -0700

    Initial Commit

---
+++ EightQueens/EightQueens.h
@@ -0,0 +1,19 @@
+//
+//  EightQueens.h
+//  EightQueens
+//
+//  Created by Steve Baker on 5/21/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EightQueens.
+FOUNDATION_EXPORT double EightQueensVersionNumber;
+
+//! Project version string for EightQueens.
+FOUNDATION_EXPORT const unsigned char EightQueensVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EightQueens/PublicHeader.h>
+
+
