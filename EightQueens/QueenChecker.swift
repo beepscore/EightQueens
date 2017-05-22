53a23b9b4aae1618106c75d8c2620618f51198df
beepscore <support@beepscore.com>
2017-05-21 17:55:56 -0700

    Delete EightQueensTests, add QueenChecker and QueenCheckerTests

---
+++ EightQueens/QueenChecker.swift
@@ -0,0 +1,17 @@
+//
+//  QueenChecker.swift
+//  EightQueens
+//
+//  Created by Steve Baker on 5/21/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class QueenChecker: NSObject {
+
+    class func foo() -> Bool {
+        return true
+    }
+
+}
---
+++ EightQueensTests/QueenCheckerTests.swift
@@ -0,0 +1,18 @@
+//
+//  EightQueensTests.swift
+//  EightQueensTests
+//
+//  Created by Steve Baker on 5/21/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import XCTest
+@testable import EightQueens
+
+class QueenCheckerTests: XCTestCase {
+    
+    func testFoo() {
+        XCTAssertTrue(QueenChecker.foo())
+   }
+    
+}
